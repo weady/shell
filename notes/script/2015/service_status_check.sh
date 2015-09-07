4b3d77ff3e9f83af386498477c8188be19cfd3e2
weady <708964732@qq.com>
2015-09-07 10:13:35 +0800

    notes

--- notes/script/2015/service_status_check.sh
+++ notes/script/2015/service_status_check.sh
@@ -1,19 +1,34 @@
 #########################################################################
-
+#
 #by wangdd 2015/6/25 
-
+#
 #########################################################################
 #!/bin/bash
 path="/homed"
-#find_start_process=`grep '^#[a-z]*[a-z]$' $path/start.sh  | awk -F'#' '{print $2}'`
 find_start_process=`grep "^_.*" $path/start.sh | awk -F" " '{print $5}' | awk -F"['.]" '{print $2}' | sed '/^$/d' | sort -u`
-for process in $find_start_process
+#find_process=`grep "^_.*" $path/start.sh | awk -F" " '{print $5}' | awk -F"['.]" '{print $2}' | sed '/^$/d' | sort -u | awk '{printf "%s ",$0}'`
+#---------------------------------------------------------------------------------------------------
+function process_status(){
+for process in $@
 do
-        command01=`ps -ef | grep "$process|*.exe" | awk -F' ' '{print $8}' | awk -F'/' '{print $2}'`
-	command02=`netstat -unltp | grep -E "LI.*$process" | head -n 1`
-        if [ ! -z "$command01" ] && [ ! -z "command02" ];then
-                echo $command02 is running
+        command01=`ps -ef | grep "$process.*exe" | awk -F' ' '{print $8}' | awk -F'/' '{print $2}'`
+	command02=`netstat -unltp | grep -E "LI.*$process"`
+	proc_count=`netstat -unltp | grep -E "LI.*$process" | wc -l`
+        if [ ! -z "$command01" ] && [ ! -z "$command02" ] && [ $proc_count -gt 1 ];then
+		echo "----------------------------------------------------"
+		echo "	 `hostname`  The $process process is running	  "  
+		echo "----------------------------------------------------"
+                echo "$command02"
         else
-                echo $process is not running,Please Check!
+                echo "ERROR! `hostname` $process is not running,Please Check!"
         fi
 done
+shift
+}
+#---------------------------------------------------------------------------------------------------
+#
+if [ $# -eq 0 ];then
+	process_status $find_start_process 
+else
+	process_status $@
+fi	
