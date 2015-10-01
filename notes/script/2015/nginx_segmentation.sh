9b8dcba8e2abfb1253e67c38be223df673c57275
weady <708964732@qq.com>
2015-10-01 16:58:11 +0800

    nginx log

--- notes/script/2015/nginx_segmentation.sh
+++ notes/script/2015/nginx_segmentation.sh
@@ -4,12 +4,12 @@
 #
 backup_log_dir="/usr/local/nginx/logs/back_log"
 [[ ! -d $backup_log_dir ]] && mkdir -p $backup_log_dir
-logrotate -vf /etc/logrotate.d/nginx >/dev/null
-date_time=`date -d "1 day ago" +%Y%m%d`
-logs=`ls $backup_log_dir`
+logrotate -vf /etc/logrotate.d/nginx >/dev/null 2>&1
+date_time=`date -d "1 day ago" +%Y%m%d%T`
+logs=`ls -l $backup_log_dir | grep "\.[0-9]$" | awk '{print $NF}'`
 for log in $logs
 do
-	file=${log%%.*}
+	file=${log%.*}
 	mv $backup_log_dir/$log $backup_log_dir/$file.$date_time
 	rm -rf $backup_log_dir/$log
 done
