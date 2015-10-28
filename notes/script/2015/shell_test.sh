8bf9fa5579311f47d2320d840c1cded14f8bfd10
weady <708964732@qq.com>
2015-10-28 09:21:52 +0800

    update

--- notes/script/2015/shell_test.sh
+++ notes/script/2015/shell_test.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #
+#主要记录shell的一些基本开发技巧
+#
 #	 wangdd 2015/7/24
-#
-# 	 while read do .... done < file
-#
 #
 #eg.1-----------------------------------------------------------
   old_IFS=$IFS
@@ -84,56 +83,31 @@ awk 'NR==FNR{a[$1]=$1"x"$2"x"$3}NR>FNR{b=substr($4,3);c=$3"x"b"x"$6;if(c==a[$3])
 awk '{for(i=1;i<=NF;i++)a[i]=$i;getline < "b.txt";for (j=1;j<=NF;j++) printf $j+a[j]" ";printf "\n"}' a.txt
 #eg.6-------------------------------------------------------------
 seq -s '#' 100 | sed -e 's/[0-9]*//g'
+arch=i486
+[[ $arch = i*86 ]] && echo "arch is x86!" 
 #eg.7-------------------------------------------------------------
-#判断交互输入的是一个数字
-function read_num(){
-	read -p "请输入一个数字:" num
-	if [[ $num =~ ^[0-9]*$ ]];then
-		echo "$num is digital"
-	else 
-		read_num
-	fi
-}
-#read_num
-while :
-do
-	read -p "请输入一个数字:" num
-	n1=`echo $num | sed 's/[0-9]//g'`
-	if [ ! -z $n1 ];then
-		echo "please input a number"
-	else
-		echo "$num is a digital"
-		break
-	fi
-done
+a0="abc"
+j=0
+b=a$j
+echo ${!b}
+eval b="$"a"$j"
+echo $b
 #eg.8-------------------------------------------------------------
-#exec 把执行过程输入到日志文件中
-d=`date +%F`
-exec >/tmp/$d.log 2>&1
-echo "Begin at `date +"%F %T"`"
-ls /tmp/adljfaljdlkfj
-cd /home
-echo "End at `date +"%F %T"`"
+expre="111.12GB"
+[[ $expre =~ GB$ ]] && echo "ok"
 #eg.9-------------------------------------------------------------
-#1至100向加
-for i in `seq 1 100`
+for num in `seq 1 2 100`
 do
-	sum=$[$sum+$i]
+        echo -n "$num|"
 done
-echo $sum
-#9*9 乘法口诀
-for i in `seq 1 9`
-do
-	for j in `seq 1 $i`
-	do
-		k=$[$i*$j]
-		echo -ne "$j x $i = $k\t"
-	done
-	echo
-done
-
-#eg.6-------------------------------------------------------------
-#eg.6-------------------------------------------------------------
-#eg.6-------------------------------------------------------------
-#eg.6-------------------------------------------------------------
-#eg.6-------------------------------------------------------------
+echo 
+#eg.10-------------------------------------------------------------
+a1=11
+a2=12
+a3=$[$a1+$a2]
+a4=`expr $a1 + $a2`
+echo $a4
+echo $a3
+#eg.11-------------------------------------------------------------
+testfile="/data1/wangdong/shell/a.txt"
+[[ -s "$testfile" ]] && echo "size gt zero" || echo "empty"
